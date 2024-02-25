d84ccd338588ccc6c879acc9a09ea49280d96dc2
Carlos Hernandez <carl.hernan34@gmail.com>
2024-02-24 20:56:17 -0500

    Add other dependency directory files
    App wasn't running without these files

---
+++ Tech-Lab-Queens-Edition/dependency/themes/themes/techlabs/header.php
@@ -0,0 +1,16 @@
+<?php
+namespace Bloomberg\TechLabs\NextJS;
+
+defined( 'ABSPATH' ) or exit;
+?>
+<!DOCTYPE html>
+<html class="bbg" <?php language_attributes(); ?>>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script>(function(H){H.className=H.className.replace(/\bbbg\b/,'bbg-js')})(document.documentElement);</script>
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+	
